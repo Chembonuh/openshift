d2a746b5bd4c0dc4a64c83f9227b31fa90a49730
Chembonuh <162596186+Chembonuh@users.noreply.github.com>
2024-08-03 15:39:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,13 @@ provider "aws" {
   region = var.region
 }
 
-module "web_server" {
-  #source = "./"
-  instance_ami = var.instance_ami
+resource "aws_instance" "regular_instance" {
+  ami           = var.instance_ami
   instance_type = var.instance_type
-  volume_size = var.volume_size
+
   tags = var.tags
+
+  root_block_device {
+    volume_size = var.volume_size
+  }
 }
