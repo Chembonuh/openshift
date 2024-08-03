2d1486a9993d97c2976536b42bea9e41ba4c66e7
Chembonuh <162596186+Chembonuh@users.noreply.github.com>
2024-08-03 17:05:57 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,3 +3,8 @@ output "instance_id" {
   description = "The ID of the web server instance"
   value       = module.web_server.instance_id
 }
+
+output "instance_public_ip" {
+  description = "The public IP address of the web server instance"
+  value       = module.web_server.instance_public_ip
+}
