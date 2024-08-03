314574c16e336255d3470956f6696a6167392c9e
Chembonuh <162596186+Chembonuh@users.noreply.github.com>
2024-08-03 16:48:16 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "instance_id" {
   description = "The ID of the instance"
-  value       = aws_instance.regular_instance.id
+  value       = aws_instance.web_server.id
 }
 
 output "instance_public_ip" {
   description = "The public IP address of the instance"
-  value       = aws_instance.regular_instance.public_ip
+  value       = aws_instance.web_server.public_ip
 }
 
 output "instance_volume_size" {
   description = "The size of the root volume"
-  value       = aws_instance.regular_instance.root_block_device[0].volume_size
+  value       = aws_instance.web_server.root_block_device[0].volume_size
 }
