54ddf24052661405d0174a136aa8890beb55e6bf
Chembonuh <162596186+Chembonuh@users.noreply.github.com>
2024-08-03 17:00:09 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,14 +1,13 @@
 output "instance_id" {
-  description = "The ID of the instance"
-  value       = aws_instance.web_server.id
+  description = "The ID of the web server instance"
+  value       = module.web_server.instance_id
 }
 
 output "instance_public_ip" {
-  description = "The public IP address of the instance"
-  value       = aws_instance.web_server.public_ip
+  description = "The public IP address of the web server instance"
+  value       = module.web_server.instance_public_ip
 }
-
 output "instance_volume_size" {
   description = "The size of the root volume"
-  value       = aws_instance.web_server.root_block_device[0].volume_size
+  value       = aws_instance.regular_instance.root_block_device[0].volume_size
 }
