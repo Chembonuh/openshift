dbe1d643df3b939f97f352b333a0ae5ef6c0ef42
Chembonuh <162596186+Chembonuh@users.noreply.github.com>
2024-08-03 16:43:13 -0400

    Update web.tf

--- web.tf
+++ web.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "regular_instance" {
+resource "aws_instance" "web_server" {
   ami           = var.instance_ami
   instance_type = var.instance_type
   key_name      = var.key_name
@@ -9,4 +9,12 @@ resource "aws_instance" "regular_instance" {
   }
 
   tags = var.tags
+}
+
+output "instance_id" {
+  value = aws_instance.web_server.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.web_server.public_ip
 }
