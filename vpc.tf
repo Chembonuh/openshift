25221c71a16392a8a8bcc0bf7859e715c864929a
Chembonuh <162596186+Chembonuh@users.noreply.github.com>
2024-08-03 17:40:29 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -38,15 +38,4 @@ resource "aws_security_group" "my_sg" {
   }
 }
 
-resource "aws_instance" "web_server" {
-  ami           = var.instance_ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
 
-  ebs_block_device {
-    device_name = "/dev/sdh"
-    volume_size = var.volume_size
-  }
-
-  tags = var.tags
-}
